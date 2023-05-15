656f422557ef4e5ff84e47f64b990cb7cc96caaf
unknown <dandihermawan87@gmail.com>
2023-05-15 17:01:05 +0700

    Baru Sampai API Food

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\API\FoodController;
+use App\Http\Controllers\API\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+//Harus login
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('user', [UserController::class, 'fetch']);
+    Route::post('user', [UserController::class, 'updateProfile']);
+    Route::post('user/photo', [UserController::class, 'updatePhoto']);
+    Route::post('logout', [UserController::class, 'logout']);
+});
+
+//tidak harus login
+Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
+
+Route::get('food', [FoodController::class, 'all']);
